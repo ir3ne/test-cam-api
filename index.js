0bbf2c8379ec4e28ce34bea9fc5487016be939ee
Irene Tomaini <irene.tomaini@refurbed.com>
2024-05-11 15:45:35 +0200

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+import { getVideo, drawVideo } from "./src/camera";
+
+(async function run () {
+	const video  = await getVideo();
+	const canvas = document.querySelector('canvas');
+	const button = document.getElementById('camera');
+
+	button.addEventListener('click', () => {
+		drawVideo(video, canvas)
+	});
+})();
